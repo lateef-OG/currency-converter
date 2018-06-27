f0370eb1fe73c32f08c0c72455fa568286e00534
Lateef_OG <lateef@cchubnigeria.com>
2018-06-27 12:11:58 +0100

    Added service worker and cached app assets

---
+++ sw.js
@@ -0,0 +1,28 @@
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+      caches.open('currencyConverter').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/css/style.css',
+          '/js/app.js',
+          'https://free.currencyconverterapi.com/api/v5/currencies'
+        ]);
+      })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+
+    console.log(event.request.url);
+    event.respondWith(
+    
+        caches.match(event.request).then(function(response) {
+        
+            return response || fetch(event.request);
+        
+        })
+        
+    );
+    
+});
